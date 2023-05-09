64c2ea22b7a7d6c10502ba7b67439f8d48912216
Berit Carvalho <beritcarvalho@gmail.com>
2023-05-09 19:40:23 -0300

    - Reset no projeto

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -3,25 +3,25 @@ export class Negociacao {
     private _quantidade;
     private _valor;
 
-    constructor(data, quantidade, valor){
+    constructor(data, quantidade, valor) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
 
-    get data(){
+    get data() {
         return this._data;
     }
 
-    get valor(){
-        return this.valor;
-    }
-
-    get quantidade(){
+    get quantidade() {
         return this._quantidade;
     }
 
-    get volume(){
+    get valor() {
+        return this._valor;
+    }
+
+    get volume() {
         return this._quantidade * this._valor;
     }
 }
