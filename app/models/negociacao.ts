07df818f15b8049245a455bb203b8afdd41a471a
Berit Carvalho <beritcarvalho@gmail.com>
2023-05-11 18:39:51 -0300

    - Alterado propriedades e formas de retorno de métodos, para entender melhor outras possibilidades

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,22 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    /* Se o construtor da classe, for informado as propriedades como parametro,
+    não há necessidade, de desclar as mesmas propriedades. na escopo da classe*/
+
+    /* Caso a propriedade for somente leitura, é possivel tornar as propriedades publicas,
+    para elas aparecerem quando o objeto for instanciado e o somente leitura impede a atribuição de valro
+    e dispensa a criação de métodos acessadores.*/
+    constructor(
+        private _data: Date,
+        public readonly quantidade: number,
+        public readonly valor: number) {};
 
     get data(): Date {
-        return this._data;
-    }
-
-    get quantidade(): number {
-        return this._quantidade;
-    }
-
-    get valor(): number {
-        return this._valor;
+        const data = new Date(this._data.getTime());
+        return data;
     }
 
     get volume(): number {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,15 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    //outra forma de declarar array Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao): void {
         this.negociacoes.push(negociacao)
     }
 
-    lista(): ReadonlyArray<Negociacao> {
+    //é possivel indicar se a lista retorna é somente leitura declarando array readonly ReadonlyArray<Negociacao>
+    lista(): readonly Negociacao[] {
         return this.negociacoes;
     }
 }
